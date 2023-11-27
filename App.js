844834470766e2f28e832adf03056af13dda9a58
antonionasc <139663010+antnasciment7@users.noreply.github.com>
2023-11-27 20:18:45 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack'
+import { useNavigation } from "@react-navigation/native";
 
-export default function App() {
+
+function telaCadastro() {
+ 
+  const navigation = useNavigation()
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={style.container}>
+      <Text>Olá, faça seu cadastro!</Text><Button />
+      <TextInput/>
+      <TextInput secureTextEntry={true}
+      />
+      <Button title="Cadastrar" onPress={() => {
+        navigation.navigate("inicioLog")
+      }}/>
+      <Button title="fazer login"
+      onPress={() => {
+        navigation.navigate("telalogin")
+
+      }}/>
     </View>
-  );
+    
+  )
+}
+function inicioLog() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Bem vindo! Parabens pelo cadastro</Text>
+      <Button title="Voltar"
+      onPress={() => {
+        navigation.navigate("telaCadastro")
+
+      }}/>
+      <Button title="Para o perfil"
+      onPress={() => {
+        navigation.navigate("perfil")
+        
+      }}/>
+    </View>
+  )
+}
+function inicial() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Bem vindo ao site.</Text>
+      <Button title="Cadastrar"
+      onPress={() => {
+        navigation.navigate("telaCadastro")
+      }}/>
+    </View>
+  )
+  }
+function telalogin() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Faça seu login</Text>
+      <Button title="Faça login"
+      onPress={() => {
+        navigation.navigate("inicioLog")
+      }}/>
+    </View>
+  )
+}
+function perfil() {
+  const navigation = useNavigation()
+  return (
+    <View style={style.container}>
+      <Text>Aqui esta seu perfil</Text>
+      <Button title="Voltar"
+      onPress={() => {
+        navigation.navigate("inicioLog")
+      }}/>
+    </View>
+  )
+  
+}
+export default function App() {
+
+  const Stack = createStackNavigator()
+  return (
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName="inicial"> 
+        <Stack.Screen name="telaCadastro" component={telaCadastro} />
+        <Stack.Screen name="inicioLog" component={inicioLog} />
+        <Stack.Screen name="inicial" component={inicial} />
+        <Stack.Screen name="telalogin" component={telalogin} />
+        <Stack.Screen name="perfil" component={perfil} />
+
+       </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center'
+  }
+})
